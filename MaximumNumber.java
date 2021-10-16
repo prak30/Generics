45785e2291e09dc1f4efe4ac4c839c1548c4daf8
pranav <prnvkatkar88@gmail.com>
2021-10-16 17:32:15 +0530

    Added:takes more than three paramets and finds maximum

--- MaximumNumber.java
+++ MaximumNumber.java
@@ -1,17 +1,24 @@
 
 public class MaximumNumber {
-	public static String findMax(String x, String y, String z) {
-		String max = x;
-		if (y.compareTo(max) > 0)
-			max = y;
-		if (z.compareTo(max) > 0)
-			max = z;
-		return max;
+	public static<E extends Comparable> void findMax(E array[]) {
+		E max = array[0];
+		 for (int i = 0; i < array.length; i++) { 
+			  if (array[i].compareTo(max) > 0)
+	               max = array[i];
+			  
+		 }
+		 System.out.println("Largest element present in given array: " + max);  
+		
 	}
 
 	public static void main(String[] args) {
-		String maxString = MaximumNumber.findMax("apple","banana","peach");
-		System.out.println("Maximum string :" +maxString);
+		//MaximumNumber m1 = new MaximumNumber();
+		Integer arrayInt[] = {1,2,5,7,10};
+		Float arrayFloat[] = {1.2f,2.6f,5.4f};
+		String arrayString[] = {"Apple","Banana","Peach"};
+		findMax(arrayInt);
+		findMax(arrayFloat);
+		findMax(arrayString);
 	}
 }
 
